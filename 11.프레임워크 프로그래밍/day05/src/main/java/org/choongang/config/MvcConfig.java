3a9e237dcbb8877a1089a55db4b6a419d1ea67f4
yeonhwa001 <chyh202@naver.com>
2024-07-18 16:55:05 +0900

    .

--- 11.프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MvcConfig.java
+++ 11.프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MvcConfig.java
@@ -60,11 +60,12 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/mypage")
                 .setViewName("mypage/index");
     }
-
+/*
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp("/WEB-INF/templates/", ".jsp");
     }
+ */
 
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertyConfigurer() {
